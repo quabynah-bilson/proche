25c12378d638a56811c8bb2691a15c2307d07e30
Quabynah Bilson Jr <84394923+quabynah-bilson@users.noreply.github.com>
2023-04-19 12:36:02 +0000

    Feature/docker (#16)

--- mobile/lib/core/utils/image.utils.dart
+++ mobile/lib/core/utils/image.utils.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Future<String> assetToBase64(String assetPath) async {
--- mobile/lib/features/shared/domain/repositories/local.storage.dart
+++ mobile/lib/features/shared/domain/repositories/local.storage.dart
@@ -2,7 +2,13 @@
 abstract class BaseLocalStorageRepository {
   const BaseLocalStorageRepository();
 
+  Future<void> saveAccessToken(String token);
+
+  Future<void> clearAccessToken();
+
   Future<String> get defaultLocale;
+
+  Future<String?> get accessToken;
 
   Future<void> setLocale(String locale);
 }
